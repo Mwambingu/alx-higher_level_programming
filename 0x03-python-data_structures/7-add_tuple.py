2b6f810adffeba0aa83bde352d0c5aadcebaab9b
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-06-06 16:43:35 +0300

    Hot fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,10 +2,15 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     if not tuple_a:
         if not tuple_b:
-            tuple_C = (0, 0)
+            tuple_c = (0, 0)
             return tuple_c
-    if len(tuple_a) == 1:
-        v_a0 = tuple_a[1]
+    if len(tuple_a) == 1 and len(tuple_b) == 1:
+        v_a0 = tuple_a[0]
+        v_a1 = 0
+        v_b0 = tuple_b[0]
+        v_b1 = 0
+    elif len(tuple_a) == 1:
+        v_a0 = tuple_a[0]
         v_a1 = 0
         v_b0 = tuple_b[0]
         v_b1 = tuple_b[1]
