c2dd280a5e08e41899959d9cc3e2832d9479e07e
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-06-09 16:04:58 +0300

    Hot Fix

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-        return list(map((lambda row: list(map((lambda x: x * x), row))), matrix))
+def weight_average(my_list=[]):
+    if my_list and len(my_list):
+        num = 0
+        dem = 0
+        for tup in my_list:
+            num += (tup[0] * tup[1])
+            dem += tup[1]
+        return (num / dem)
+    return 0
