0f5ba4069fe1b4840848bde170a75b9e11f2376b
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-06-20 21:59:37 +0300

    Hot Fix Task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,6 +4,7 @@ def safe_print_division(a, b):
         result = a / b
     except ZeroDivisionError:
         result = None
+        return result
     finally:
         print("{} {}".format("Inside result:", result))
-        return result
+    return result
