ce6ae8bfd3f2e769fa7e50aecb304f4494166631
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-06-20 19:30:59 +0300

    Hot fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,6 @@ def safe_print_list(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
         print("{}".format(""))
         return i+1
-    except:
+    except IndexError:
         print("{}".format(""))
         return i
