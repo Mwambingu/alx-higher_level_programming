af905f399cbad804c21359c7598f2d22049fe88b
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-06-20 21:07:19 +0300

    Hot Fix task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if not my_list:
-        return 0
-    if x == 0:
-        return 0
+    count = 0
     try:
         for i in range(0, x):
             print("{}".format(my_list[i]), end="")
+            count += 1
         print("{}".format(""))
-        return i+1
+        return count
     except IndexError:
         print("{}".format(""))
-        return i
+        return count
