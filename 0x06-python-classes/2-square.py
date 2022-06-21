851239b32ae785906b8661c3f973acb8dcd5b4d6
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-06-21 22:38:24 +0300

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Module with a class Square """
+
+
+class Square:
+    """A class that defines a square.
+
+
+    Attributes:
+        __size (int): size of the square's side
+    """
+    def __init__(self, size=0):
+        """Initializes the square.
+        Args:
+            size (int): size of the square's side
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
