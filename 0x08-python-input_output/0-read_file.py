5ee531ec4874e3765e4cf967bf2b9b1d7c770dd0
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-07-05 21:03:23 +0300

    Hot Fix

--- 0x08-python-input_output/0-read_file.py
+++ 0x08-python-input_output/0-read_file.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 """
-Contains a function that reads & returns a files contents
+Contains a function that reads & prints a files contents
 """
 
 
 def read_file(filename=""):
-    """Function that reads a file and returns the contents
+    """Function that reads a file and prints the contents
     read.
 
     Args:
         filename (str): the string that contains the filename
-
-    Return: content of file
     """
     with open(filename, encoding="utf8") as f:
-        return f.read()
+        print(f.read(), end="")
