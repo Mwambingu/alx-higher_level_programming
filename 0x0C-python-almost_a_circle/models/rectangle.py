da040c17249df74496935b9e2bda7fdd7b761eee
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-07-11 18:08:36 +0300

    Hot fixes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -25,6 +25,7 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
         super().__init__(id)
+
     @property
     def width(self):
         """Set/get the width of the Rectangle."""
@@ -76,3 +77,7 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """returns area of rectangle"""
+        return self.__height * self.__width
