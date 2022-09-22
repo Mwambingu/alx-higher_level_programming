7bc726de9e43137fc1778c25a48fe2f2ff563783
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-09-22 19:09:54 +0300

    Hot fix

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends POST request with given variable parameters.
-curl -X POST -F "email=test@gmail.com" -F "subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
