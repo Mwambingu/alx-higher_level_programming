e34bf937c4145150ab42791a456e83aab6ad4035
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-09-23 18:28:36 +0300

    Hot fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,6 +4,7 @@ Contains a python script that fetches https://alx-intranet.hbtn.io/status
 """
 import urllib.request
 
+
 if __name__ == "__main__":
     request = urllib.request.Request("https://intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
